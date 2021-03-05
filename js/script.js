d676215b9586c0db19d271b6ace124b26ecb18c5
Filippo Garota <filogarota101@gmail.com>
2021-03-05 16:23:52 +0100

    somma dei prezzi

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var nameBurger = document.getElementById('burger-name');
-var calculate = document.getElementById('totale');
+var calculate = document.getElementById('calculate');
 
 calculate.addEventListener('click',
  function() {
@@ -7,9 +7,16 @@ calculate.addEventListener('click',
    if (nameBurger != '') {
     // creo una variabile per gli ingredienti
     var ingredienti = document.getElementsByClassName('ingredients')
-
-   } else {
-     alert('Attenzione, inserire il nome dell'Hamburger);
-   }
- }
+    // scorro gli ingredienti e sommo quelli selezionati
+    var somma = 50;
+    for (var i = 0; i < ingredienti.length; i++) {
+      if (ingredienti[i].checked == 1) {
+        somma += parseInt(ingredienti[i].value);
+      }
+    }
+    console.log(somma);
+  } else {
+    alert("Attenzione, inserire il nome dell'Hamburger!!");
+  }
+}
 );
