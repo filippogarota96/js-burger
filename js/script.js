3d99bf7cad5a7ba3d60d8dd8fecc15176b57bfad
Filippo Garota <filogarota101@gmail.com>
2021-03-05 17:02:23 +0100

    Stampo il costo totale del panino sulla pagina

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var nameBurger = document.getElementById('burger-name');
 var calculate = document.getElementById('calculate');
 var coupon = document.getElementById('discount-coupon')
+var prezzoTotale = document.getElementById('totale');
+var couponList = ["garo20", "456ABC", "buger20"]
+
+
 calculate.addEventListener('click',
  function() {
    // verifico che il nome sia stato inserito
@@ -16,17 +20,16 @@ calculate.addEventListener('click',
       }
       i++;
     }
-    // creo un array di coupons
-    var couponList = ["garo20", "456ABC", "buger20"]
+    // verifico che il coupon sia valido e applico lo sconto
     for (var j = 0; j < couponList.length; j++) {
       if (coupon.value == couponList[j]) {
         somma -= somma * 0.2;
       }
     }
-    console.log(somma);
+    // stampo il prezzo scontato nella pagina
+    prezzoTotale.innerHTML = somma;
   } else {
     alert("Attenzione, inserire il nome dell'Hamburger!!");
   }
-
 }
 );
