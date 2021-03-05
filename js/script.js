7adf8ca8bbcac4bc3f81d203244594b69137f373
Filippo Garota <filogarota101@gmail.com>
2021-03-05 16:04:38 +0100

    Aggiunto js evento

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 var nameBurger = document.getElementById('burger-name');
 var calculate = document.getElementById('totale');
+
+calculate.addEventListener('click',
+ function() {
+   // verifico che il nome sia stato inserito
+   if (nameBurger != '') {
+    // creo una variabile per gli ingredienti
+    var ingredienti = document.getElementsByClassName('ingredients')
+
+   } else {
+     alert('Attenzione, inserire il nome dell'Hamburger);
+   }
+ }
+);
