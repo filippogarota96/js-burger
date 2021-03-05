8cbfbf1279185a84109d927fc1850ccdfdc3fbd3
Filippo Garota <filogarota101@gmail.com>
2021-03-05 16:53:14 +0100

    Calcolo del totale meno lo sconto

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var nameBurger = document.getElementById('burger-name');
 var calculate = document.getElementById('calculate');
-
+var coupon = document.getElementById('discount-coupon')
 calculate.addEventListener('click',
  function() {
    // verifico che il nome sia stato inserito
-   if (nameBurger != '') {
+   if (nameBurger.value != '') {
     // creo una variabile per gli ingredienti
     var ingredienti = document.getElementsByClassName('ingredients')
     // scorro gli ingredienti e sommo quelli selezionati
@@ -16,11 +16,17 @@ calculate.addEventListener('click',
       }
       i++;
     }
+    // creo un array di coupons
+    var couponList = ["garo20", "456ABC", "buger20"]
+    for (var j = 0; j < couponList.length; j++) {
+      if (coupon.value == couponList[j]) {
+        somma -= somma * 0.2;
+      }
+    }
     console.log(somma);
-
   } else {
-
     alert("Attenzione, inserire il nome dell'Hamburger!!");
   }
+
 }
 );
